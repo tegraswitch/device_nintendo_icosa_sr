73b1819dac778209cd6f02e4a3085f2778282ec8
Billy Laws <blaws05@gmail.com>
2021-01-09 18:11:05 +0000

    Add serdev joycon driver modules
    Change-Id: I25659a940d3367162a089749b966d991d8855509

--- initfiles/lkm_loader_sr.sh
+++ initfiles/lkm_loader_sr.sh
@@ -34,6 +34,14 @@ fi
 
 /vendor/bin/log -t "$scriptName" -p i "Early Loading LKM SR modules completed"
 
+# Joycons (make sure to load after pwm_fan to avoid excessive fanspin!)
+/vendor/bin/log -t "$scriptName" -p i "Loading JoyCon serdev modules started"
+
+do_insmod /vendor/lib/modules/crc8.ko
+do_insmod /vendor/lib/modules/joycon-serdev.ko
+
+/vendor/bin/log -t "$scriptName" -p i "Loading JoyCon serdev modules completed"
+
 }
 
 #===================================================================================
