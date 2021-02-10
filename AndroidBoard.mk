43d4027c806aee6c29c292cfa54ecc29ae1e16b2
Aaron Kling <webgeek1234@gmail.com>
2021-02-10 12:00:53 -0600

    Only package the icosa dtb

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
--include device/nvidia/foster/AndroidBoard.mk
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+
+DTB_TARGETS := tegra210-icosa.dtb
+INSTALLED_DTB_TARGETS := $(DTB_TARGETS:%=$(PRODUCT_OUT)/install/%)
+$(INSTALLED_DTB_TARGETS): $(INSTALLED_KERNEL_TARGET) | $(ACP)
+	echo -e ${CL_GRN}"Copying individual DTBs"${CL_RST}
+	@mkdir -p $(PRODUCT_OUT)/install
+	cp $(@F:%=$(KERNEL_OUT)/arch/arm64/boot/dts/%) $(PRODUCT_OUT)/install/
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTB_TARGETS)
+endif
+
+EKS_DAT_SYMLINK := $(TARGET_OUT_VENDOR)/app/eks2/eks2.dat
+$(EKS_DAT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	$(hide) ln -sf /data/vendor/eks2/ek2.dat $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EKS_DAT_SYMLINK)
