671ef84402778bccd7c7a25f5a225de2c986f0ed
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-17 15:49:09 -0500

    properties: Remove deprecated ro.gamestream.display.optimize
    * No longer on foster/darcy/mdarcy stock Android 9.0 or 11.0.
    
    Change-Id: I4b4e347a3a6c1ea40b6926bea08bba80a93c2390

--- system_prop.mk
+++ system_prop.mk
@@ -2,10 +2,6 @@
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     persist.sys.NV_ECO.IF.CHARGING=false
 
-# Gamestreaming specific properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.gamestream.display.optimize=1
-
 # Force sw compositing
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.tegra.composite.policy=composite-always \
