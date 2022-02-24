ed92f6c94485e32083d7fc91a91d7050e279bcd6
Thomas Makin <halorocker89@gmail.com>
2022-02-24 16:21:00 -0500

    product: set shipping api level
    - Includes product_launched_with_l makefile
    - Resolves treble issues

--- full_icosa_sr.mk
+++ full_icosa_sr.mk
@@ -15,6 +15,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 $(call inherit-product, device/nintendo/icosa_sr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
